77d99430f32fb9dae08866988d8f4189efc95739
Daniel Domínguez <danieldominguez05@gmail.com>
2017-02-15 21:22:11 +0100

    Trabajo en la ultima clase

--- app/controllers/main_view_controller.rb
+++ app/controllers/main_view_controller.rb
@@ -2,22 +2,22 @@ class MainViewController < ApplicationController
   def index
     points = PointOfInterest.all
 
-    @default_location = default_location points
-    @markers = create_markers points
-    p @markers
+    @default_location = _default_location points
+    @markers = _create_markers points
   end
 
   private
 
-  def default_location(points)
+  def _default_location(points)
     points[0].location
   end
 
-  def create_markers(points)
+  def _create_markers(points)
     points.map do |point|
       {
         latlng: [point.location.longitude, point.location.latitude],
-        popup: point.name
+        #popup: point.name
+        , imagePath: ''
       }
     end
   end
